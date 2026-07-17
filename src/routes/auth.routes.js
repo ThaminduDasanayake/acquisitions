3d5ef1fbfdccdffe18e0696c41bf9273c10cce81
Thamindu Dasanayake <thamindudasanayake@gmail.com>
2026-07-17 19:41:23 +0530

    Implement user registration with password hashing, database integration, and JWT authentication

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import { signUp } from '#controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/sign-up', async (req, res) => {
-  res.send('POST /api/auth/sign-up');
-});
+router.post('/sign-up', signUp);
 
 router.post('/sign-in', async (req, res) => {
   res.send('POST /api/auth/sign-in');
